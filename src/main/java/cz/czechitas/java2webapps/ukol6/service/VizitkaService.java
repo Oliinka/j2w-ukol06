899412510dc539557f3349b04000aa1fac730a9e
Olinka <olga.titzenthaler@gmail.com>
2024-06-01 20:49:04 +0200

    work in progress: -Vizitka -VizitkaCOntroller -VizitkaService

--- src/main/java/cz/czechitas/java2webapps/ukol6/service/VizitkaService.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/service/VizitkaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class VizitkaService {
     private final List<Vizitka> seznamVizitek = new ArrayList<>();
