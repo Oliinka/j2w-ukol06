b0ea7da6c840e85ac782fd933abbba3765369b28
Olinka <olga.titzenthaler@gmail.com>
2024-06-08 14:29:08 +0200

    work in progress: -Vizitka -VizitkaCOntroller -VizitkaService

--- src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -2,9 +2,6 @@ package cz.czechitas.java2webapps.ukol6.controller;
 
 import cz.czechitas.java2webapps.ukol6.entity.Vizitka;
 import cz.czechitas.java2webapps.ukol6.repository.VizitkaRepository;
-
-
-import jakarta.validation.Valid;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -12,109 +9,18 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-
-/**
- * Kontroler obsluhující zobrazování vizitek.
- */
-@Controller
-public class VizitkaController {
-
-    private final VizitkaRepository vizitkaRepository;
-    public VizitkaController(VizitkaRepository vizitkaRepository){
-        this.vizitkaRepository= vizitkaRepository;
-    }
-
-
-    @InitBinder
-    public void nullStringBinding(WebDataBinder binder) {
-        //prázdné textové řetězce nahradit null hodnotou
-        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
-    }
-
-    //opraveno
-    @GetMapping("/")
-    public ModelAndView seznam() {
-    //TODO načíst seznam osob
-        return new ModelAndView("seznam")
-                .addObject("vizitky", vizitkaRepository.findAll());
-}
-
-
-    @GetMapping("/nova")
-    public ModelAndView nova() {
-        return new ModelAndView("detail")
-                .addObject("vizitka", new Vizitka());
-    }
-
-    @PostMapping("/nova")
-    public String pridat(@ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "detail";
-        }
-        //uložit údaj o nové osobě
-        vizitkaRepository.save(vizitka);
-        return "redirect:/";
-    }
-
-    @GetMapping("/{id:[0-9]+}")
-    public ModelAndView detail(@PathVariable long id) {
-        //TODO načíst údaj o osobě
-        Optional<Vizitka> osoba=vizitkaRepository.findById(id);
-        if(osoba.isEmpty()){
-            return new ModelAndView("redirect:/");
-        }
-        return new ModelAndView("detail")
-                .addObject("osoba", osoba.get());
-    }
-
-    @PostMapping("/{id:[0-9]+}")
-    public String ulozit(@ModelAttribute("osoba") @Valid Vizitka vizitka, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "detail";
-        }
-        //TODO uložit údaj o osobě
-        vizitkaRepository.save(vizitka);
-        return "redirect:/";
-    }
-
-    @PostMapping(value = "/{id:[0-9]+}", params = "akce=smazat")
-    public String smazat(@PathVariable long id) {
-        //TODO smazat údaj o osobě
-        vizitkaRepository.deleteById(id);
-        return "redirect:/";
-    }
-
-}
-
-
-/* package cz.czechitas.java2webapps.ukol6.controller;
-
-import cz.czechitas.java2webapps.ukol6.entity.Vizitka;
-import cz.czechitas.java2webapps.ukol6.repository.VizitkaRepository;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import java.util.Optional;
 
 @Controller
 public class VizitkaController {
+
     private final VizitkaRepository vizitkaRepository;
 
-
-    @Autowired
-    public VizitkaController (VizitkaRepository vizitkaRepository){
+    public VizitkaController(VizitkaRepository vizitkaRepository) {
         this.vizitkaRepository = vizitkaRepository;
     }
+
     @InitBinder
     public void nullStringBinding(WebDataBinder binder) {
         binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
@@ -123,19 +29,9 @@ public class VizitkaController {
     @GetMapping("/")
     public ModelAndView seznam() {
         return new ModelAndView("seznam")
+                //slovo v tomto případě "vizitka" je clovo co nastavuju ve ftlh souborech
                 .addObject("vizitka", vizitkaRepository.findAll());
-    }
 
-    @GetMapping("/{id}")
-    public Object vizitka(@PathVariable int id) {
-        Optional<Vizitka> optionalVizitka = vizitkaRepository.findById(id);
-        if (optionalVizitka.isPresent()){
-            return new ModelAndView("vizitka")
-                    .addObject("vizitka", optionalVizitka.get());
-        }
-        else {
-            return ResponseEntity.notFound().build();
-        }
     }
 
     @GetMapping("/nova")
@@ -144,15 +40,37 @@ public class VizitkaController {
                 .addObject("vizitka", new Vizitka());
     }
 
-
     @PostMapping("/nova")
     public String pridat(@ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
-      if (bindingResult.hasErrors()) {
-           return "formular";
-       }
+        if (bindingResult.hasErrors()) {
+            return "formular";
+        }
         vizitkaRepository.save(vizitka);
-        return "redirect:/";}
+        return "redirect:/";
+    }
 
+    @GetMapping("/vizitka/{id}")
+    public ModelAndView detail(@PathVariable long id) {
+        Optional<Vizitka> vizitka = vizitkaRepository.findById(id);
+        if (vizitka.isEmpty()) {
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("vizitka")
+                .addObject("vizitka", vizitka.get());
+    }
+
+    @PostMapping("/{id}")
+    public String ulozit(@ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "formular";
+        }
+        vizitkaRepository.save(vizitka);
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/{id}", params = "akce=smazat")
+    public String smazat(@PathVariable long id) {
+        vizitkaRepository.deleteById(id);
+        return "redirect:/";
+    }
 }
-*/
-
--- src/main/java/cz/czechitas/java2webapps/ukol6/entity/Vizitka.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/entity/Vizitka.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
+
+import java.beans.Transient;
 import java.util.Objects;
 
 
@@ -22,62 +20,65 @@ public class Vizitka {
     private Long id;
 
     @Length(max = 100)
-    //@NotBlank
+    @NotBlank
     private String celeJmeno;
 
     @Length(max = 100)
-    //@NotBlank
+    @NotBlank
     private String firma;
 
     @Length(max = 100)
-    //@NotBlank
+    @NotBlank
     private String ulice;
 
     @Length(max = 100)
-    //@NotBlank
+    @NotBlank
     private String obec;
 
     @Length(max = 100)
-    //@NotBlank
+    @NotBlank
     private String psc;
 
     @Length(max = 100)
+    @NotBlank
     private String email;
 
     @Length(min = 9, max = 13)
+    @NotBlank
     @Pattern(regexp = "\\+?\\d+")
     private String telefon;
 
     @Length(max = 100)
+    @NotBlank
     private String web;
 
-    public Vizitka() {
+
+    public Long getId() {
+        return id;
     }
 
-    public Vizitka(String celeJmeno, String firma, String ulice, String obec, String psc, String email, String telefon, String web) {
-        this.celeJmeno = Objects.requireNonNull(celeJmeno);
-        this.firma = Objects.requireNonNull(firma);
-        this.ulice = Objects.requireNonNull(ulice);
-        this.obec = Objects.requireNonNull(obec);
-        this.psc = Objects.requireNonNull(psc);
-        this.email = email;
-        this.telefon = telefon;
-        this.web = web;
+    public void setId(Long id) {
+        this.id = id;
     }
+
 
     public String getCeleJmeno() {
+
         return celeJmeno;
     }
 
-    public void setceleJmeno(String jmeno) {
-        this.celeJmeno = Objects.requireNonNull(jmeno);
+    public void setCeleJmeno(String celeJmeno) {
+
+        this.celeJmeno = Objects.requireNonNull(celeJmeno);
     }
 
     public String getFirma() {
+
         return firma;
     }
 
     public void setFirma(String firma) {
+
         this.firma = Objects.requireNonNull(firma);
     }
 
@@ -90,46 +91,52 @@ public class Vizitka {
     }
 
     public String getObec() {
+
         return obec;
     }
 
     public void setObec(String obec) {
+
         this.obec = Objects.requireNonNull(obec);
     }
 
     public String getPsc() {
+
         return psc;
     }
 
     public void setPsc(String psc) {
+
         this.psc = Objects.requireNonNull(psc);
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getTelefon() {
+
         return telefon;
     }
 
     public void setTelefon(String telefon) {
+
         this.telefon = telefon;
     }
 
     public String getWeb() {
+
         return web;
     }
 
     public void setWeb(String web) {
-        this.web = web;
-    }
 
-    public String getCelaAdresa() {
-        return ulice + ", " + obec + ", " + psc;
+        this.web = web;
     }
 }
